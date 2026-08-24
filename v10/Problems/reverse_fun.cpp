9eda4c84ad410c41433b751af06124176c67c50b
dayasagar543 <dayasagar.543@gmail.com>
2026-08-24 16:21:23 +0530

    added the code in the single number problem and reverse_function problem

--- v10/Problems/reverse_fun.cpp
+++ v10/Problems/reverse_fun.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+vector<int> reverse(vector<int>&vec);
+void swap(int &a , int &b);
 int main()
 {
     vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
-
-   
+    vector<int> rev_vec=reverse(vec);
+    for(int i=0;i<rev_vec.size()-1;i++){
+        cout<< rev_vec[i]<<endl;
+    }
+    
+}
+vector<int> reverse(vector<int>&vec){
+    int start=0,end=vec.size()-1;
+    while(start<end){
+        swap(vec[start],vec[end]);
+        start++;
+        end--;
+    }
+return  vec;
+}
+void swap(int &a,int &b){
+    int c=a;
+    a=b;
+    b=c;
 }
